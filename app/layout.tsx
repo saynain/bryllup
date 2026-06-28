dc95725ec0aa82214448153e40d7ea8f80ca60d5
saynain <sindre.ryland@gmail.com>
2026-06-28 16:19:09 +0200

    Remove link preview image metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,22 +25,8 @@ export const metadata: Metadata = {
     description: "Invitasjon til bryllupet vårt 15. august 2026",
     url: "https://bryllup.rylands.no",
     siteName: "Silje & Sindre",
-    images: [
-      {
-        url: "/photos/Bilde_1.jpg",
-        width: 2524,
-        height: 3785,
-        alt: "Silje & Sindre",
-      },
-    ],
     locale: "nb_NO",
     type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Silje & Sindre - Vi gifter oss!",
-    description: "Invitasjon til bryllupet vårt 15. august 2026",
-    images: ["/photos/Bilde_1.jpg"],
   },
 };
 
